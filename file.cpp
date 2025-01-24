f9a133bfe04dfc9f0591c4251ee2d1007149bc29
Noah Melton <nmelton3@ut.utm.edu>
2025-01-24 14:43:35 -0600

    this is the finished file

--- file.cpp
+++ file.cpp
@@ -4,10 +4,30 @@
 
 using namespace std;
 
+
+int sum(int n);
+
 int main() {
 
-  cout << "hello world";
+
+  int n = 0; 
+  cout << "enter a number: ";
+  cin >> n;
+
+  cout << sum(n); 
 
   return 0;
 
+} 
+
+
+int sum(int n) {
+
+  int total = 0;
+  
+  for(int i = 0; i <= n; i++){
+    total += i;
+  }
+
+  return total; 
 } 
